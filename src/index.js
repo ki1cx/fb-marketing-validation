cf54ea9a85ca19bb55b202957a586110ea69d2db
Ki Chang <kiwchang@gmail.com>
2018-04-02 13:33:24 -0700

    add index file

---
+++ src/index.js
@@ -0,0 +1,44 @@
+import {
+  validateCallToAction,
+  CallToActionMapping
+} from 'callToActionValidation';
+import {
+  CreativeMappingByAdFormat,
+  CreativeMappingByObjective,
+  validateAdFormat
+} from "creativeValidation";
+import {
+  calculateMediaRequirements,
+  validateMedia,
+  MediaRequirements,
+} from 'mediaValidation';
+import {
+  PlacementValidations,
+  PlacementMapping,
+} from 'placementValidation';
+
+export {
+  validateCallToAction,
+  CallToActionMapping,
+  CreativeMappingByAdFormat,
+  CreativeMappingByObjective,
+  validateAdFormat,
+  calculateMediaRequirements,
+  validateMedia,
+  MediaRequirements,
+  PlacementValidations,
+  PlacementMapping,
+}
+
+export default {
+  validateCallToAction,
+  CallToActionMapping,
+  CreativeMappingByAdFormat,
+  CreativeMappingByObjective,
+  validateAdFormat,
+  calculateMediaRequirements,
+  validateMedia,
+  MediaRequirements,
+  PlacementValidations,
+  PlacementMapping,
+};
